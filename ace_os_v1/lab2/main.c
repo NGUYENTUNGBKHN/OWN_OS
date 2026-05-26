d0dc9fc8bcbcf3d8ffb921ef938fd71a5f5a50f8
Nguyen-Thanh-Tung <nguyenthanhtung8196@gmail.com>
2026-05-26 17:25:17 +0900

    add lab2

---
+++ ace_os_v1/lab2/main.c
@@ -0,0 +1,39 @@
+/**
+ * @file       main.c
+ * @brief      
+ * @date       2026/05/26
+ * @author     [Gentantun] (nguyenthanhtung8196@gmail.com)
+ * @details    
+ * @ref        
+ * @copyright  Copyright (c) 2026 RoboTun
+*/
+#include <stdio.h>
+#include "activate.h"
+/**
+ * @brief      User task function
+ * 
+*/
+void usertask(void)
+{
+    printf("User task\n");
+    syscall();
+    while(1);
+}
+
+int main(void)
+{
+    // Simulate user task stack (not used in this simple example)
+    unsigned int usertask_stack[256];
+    // Point to the top of the user task stack
+    unsigned int *usertask_stack_start = usertask_stack + 256 - 16;
+    usertask_stack_start[8] = (unsigned int)usertask; // Set the return address to usertask
+
+    printf("ACE OS Lab 2!\n");
+    activate(usertask_stack_start); // Start the user task
+    printf("Back to main\n");
+    
+    while(1) 
+    {
+
+    }
+}
