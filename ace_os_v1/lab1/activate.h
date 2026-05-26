285d32ec93a5bcc100bec26ce6ff09b436a30b11
Nguyen-Thanh-Tung <nguyenthanhtung8196@gmail.com>
2026-05-26 15:11:40 +0900

    context switch 1

---
+++ ace_os_v1/lab1/activate.h
@@ -0,0 +1,16 @@
+/**
+ * @file       activate.h
+ * @brief      
+ * @date       2026/05/26
+ * @author     [Gentantun] (nguyenthanhtung8196@gmail.com)
+ * @details    
+ * @ref        
+ * @copyright  Copyright (c) 2026 RoboTun
+*/
+#ifndef ACTIVATE_H
+#define ACTIVATE_H  
+
+
+void activate(unsigned int *stack_top);
+
+#endif /* ACTIVATE_H */
--- ace_os_v1/lab1/main.c
+++ ace_os_v1/lab1/main.c
@@ -1,12 +1,36 @@
+/**
+ * @file       main.c
+ * @brief      
+ * @date       2026/05/26
+ * @author     [Gentantun] (nguyenthanhtung8196@gmail.com)
+ * @details    
+ * @ref        
+ * @copyright  Copyright (c) 2026 RoboTun
+*/
 #include <stdio.h>
+#include "activate.h"
+/**
+ * @brief      User task function
+ * 
+*/
+void usertask(void)
+{
+    printf("User task\n");
+    while(1);
+}
 
 int main(void)
 {
-    unsigned tick = 0;
+    // Simulate user task stack (not used in this simple example)
+    unsigned int usertask_stack[256];
+    // Point to the top of the user task stack
+    unsigned int *usertask_stack_start = usertask_stack + 256 - 16;
+    usertask_stack_start[8] = (unsigned int)usertask; // Set the return address to usertask
 
-    for (;;) {
-        printf("hello from lab1 over ITM: %u\n", tick++);
-        for (volatile unsigned delay = 0; delay < 1000000U; ++delay) {
-        }
+    printf("ACE OS Lab 1!\n");
+    activate(usertask_stack_start); // Start the user task
+    while(1) 
+    {
+
     }
 }
