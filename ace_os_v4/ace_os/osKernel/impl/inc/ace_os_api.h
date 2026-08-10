97cd04d95332eb0c309f3139242038e30dfe594d
Nguyen-Thanh-Tung <nguyenthanhtung8196@gmail.com>
2026-08-10 18:02:26 +0900

    uu

--- ace_os_v4/ace_os/osKernel/impl/inc/ace_os_api.h
+++ ace_os_v4/ace_os/osKernel/impl/inc/ace_os_api.h
@@ -20,12 +20,79 @@ extern "C"
 #include "ace_os_port.h"
 
 
+/*
+************************************************************************************************************************
+************************************************************************************************************************
+*                                       A C E - O S   D E F I N I T I O N
+************************************************************************************************************************
+************************************************************************************************************************
+*/
+#define ACE_OS_STACK_FILL                   ((ULONG)  0xEFEFEFEFUL)
+
+/* Thread execution state values.  */
+
+#define ACE_OS_READY                        ((UINT) 0)
+#define ACE_OS_COMPLETED                    ((UINT) 1)
+#define ACE_OS_TERMINATED                   ((UINT) 2)
+#define ACE_OS_SUSPENDED                    ((UINT) 3)
+#define ACE_OS_SLEEP                        ((UINT) 4)
+#define ACE_OS_QUEUE_SUSP                   ((UINT) 5)
+#define ACE_OS_SEMAPHORE_SUSP               ((UINT) 6)
+#define ACE_OS_EVENT_FLAG                   ((UINT) 7)
+#define ACE_OS_BLOCK_MEMORY                 ((UINT) 8)
+#define ACE_OS_BYTE_MEMORY                  ((UINT) 9)
+#define ACE_OS_IO_DRIVER                    ((UINT) 10)
+#define ACE_OS_FILE                         ((UINT) 11)
+#define ACE_OS_TCP_IP                       ((UINT) 12)
+#define ACE_OS_MUTEX_SUSP                   ((UINT) 13)
+#define ACE_OS_PRIORITY_CHANGE              ((UINT) 14)
+
+/*
+************************************************************************************************************************
+************************************************************************************************************************
+*                           A C E - O S   S T R U C T U R E   D E F I N I T I O N
+************************************************************************************************************************
+************************************************************************************************************************
+*/
+
 typedef struct ACE_OS_THREAD_STRUCT
 {
-    ULONG       ace_os_thread_id;         /* Control block ID */
-      
+    ULONG       ace_os_thread_id;                       /* Control block ID */
+    
+    VOID        *ace_os_thread_stack_ptr;               /* ACE OS's stack pointer */
+    VOID        *ace_os_thread_stack_start;             /* ACE_OS's stack start address */
+    VOID        *ace_os_thread_stack_end;               /* ACE_OS's stack end address */
+    ULONG       ace_os_thread_stack_size;
+
+    struct ACE_OS_THREAD_STRUCT
+                *ace_os_thread_ready_next,
+                *ace_os_thread_ready_previous;
+    
+    CHAR        *ace_os_thread_name;
+    UINT        ace_os_thread_priority;
+    UINT        ace_os_thread_state;
+
+    VOID        (*ace_os_thread_entry)(ULONG id);
+    ULONG       ace_os_thread_entry_params;
 
 }ACE_OS_THREAD;
+
+/*
+************************************************************************************************************************
+************************************************************************************************************************
+*                           A C E - O S   
+************************************************************************************************************************
+************************************************************************************************************************
+*/
+
+#define ACE_OS_MEMSET(a, b, c)                          memset((a), (b), (c))
+
+#define ACE_OS_UCHAR_POINTER_ADD(a, b)                  (((UCHAR *)(a)) + ((UINT)(b)))
+
+/* Convert void between char */
+#define ACE_OS_VOID_TO_CHAR_POINTER_CONVERT(tmp)        ((UCHAR *)((VOID *)(tmp)))
+#define ACE_OS_CHAR_TO_VOID_POINTER_CONVERT(tmp)        ((VOID *)(tmp))
+
 
 #ifdef __cplusplus
 }
--- ace_os_v4/ace_os/osKernel/impl/src/ace_os_thread.c
+++ ace_os_v4/ace_os/osKernel/impl/src/ace_os_thread.c
@@ -48,14 +48,68 @@
 **                                             FUNCTION DEFINITIONS
 ***************************************************************************************************************/
 
-UINT ace_os_thread_create(void)
-{
+/*
+****************************************************************************************************************
+                                            CREATE A THREAD
 
+ * @brief      
+ * @param      thread_ptr    
+ * @param      name_ptr    
+ * @param      entry_function    
+ * @param      entry_input    
+ * @param      stack_start    
+ * @param      stack_size    
+ * @param      priority    
+ * @param      time_slice    
+ * @return     UINT
+****************************************************************************************************************
+*/
+UINT ace_os_thread_create(ACE_OS_THREAD *thread_ptr, 
+                          CHAR *name_ptr,
+                          VOID (*entry_function)(ULONG id),
+                          ULONG entry_input,
+                          VOID *stack_start,
+                          ULONG stack_size,
+                          UINT priority,
+                          ULONG time_slice  
+                        )
+{
+    UCHAR *temp_ptr;
+
+
+    /* Initilaize stack frame */
+    ACE_OS_MEMSET(stack_start, ((UCHAR)ACE_OS_STACK_FILL), stack_size);
+
+    /* Initialize thread control block */
+    ACE_OS_MEMSET(thread_ptr, 0, sizeof(ACE_OS_THREAD));
+
+    /* Place the supplied parameters into the thread's control block. */
+    thread_ptr->ace_os_thread_name          = name_ptr;
+    thread_ptr->ace_os_thread_entry         = entry_function;
+    thread_ptr->ace_os_thread_entry_params  = entry_input;
+    thread_ptr->ace_os_thread_priority      = priority;
+    thread_ptr->ace_os_thread_stack_start   = stack_start;
+    thread_ptr->ace_os_thread_stack_size    = stack_size;
+
+    /* Calculate the end of the thread's stack area */
+    temp_ptr = ACE_OS_VOID_TO_CHAR_POINTER_CONVERT(stack_start);
+    temp_ptr = (ACE_OS_UCHAR_POINTER_ADD(temp_ptr, (stack_size - ((ULONG)1))));
+    thread_ptr->ace_os_thread_stack_end = ACE_OS_CHAR_TO_VOID_POINTER_CONVERT(temp_ptr);
+
+    /* Set State */
+    thread_ptr->ace_os_thread_state = ACE_OS_SUSPENDED;
+
+    ace_os_thread_stack_build();
+
+    // thread_ptr->ace_os_thread_id = ACE_
+
+    /* Place the thread on the list of created threads */
+    
 }
 
 UINT ace_os_thread_delete(void)
 {
-
+    
 }
 
 UINT ace_os_thread_entry_exit_notify(void)
